e1fcf059ae24e562b88889805485b502db0fa6b2
khobs99 <khobday99@gmail.com>
2020-01-23 22:39:05 -0500

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,26 +1,65 @@
-/*********************************
- * Bushel code
- *  Created by Kenneth Hobday       01/23/2020
- *********************************/
-
 #include<iostream>
+#include<cstdlib>
+#include<fstream>
+#include<string>
+
+
 using namespace std;
-
-#include "Person.h"
-
-//Names constructor
-//Postcondition both first name and last name initialized to zero
+#include "Person.h";
 
 
-person::person()
+void pop(string *xp, string *yp);
+void sort(string name[20], int count);
+
+int main()
 {
-    first_name[CAPACITY] = "";
-    last_name[CAPACITY] = "";
+	class person names[20];
+	ifstream infile;
+	ofstream outfile;
+	string filename;
+	string name[20];
+
+	cin >> filename;
+	infile.open(filename + ".txt");
+	outfile.open("Person_New.txt");
+
+	if (infile.fail())
+	{
+		cout << "The file requested did not open" << endl;
+		exit(1);
+	}
+	while (!infile.eof())
+	{
+		names[20].get(infile);
+		
+	}
+	sort(name, 20);
+
+	
+
+
+
 
 }
-
-bool person::get(istream& in)
+void pop(string *xp, string *yp)
 {
-    in >> first_name >> last_name >> age;
+	string temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+void sort(string name[20], int count)
+{
+	int i, j;
+	for (i = 0; i < count - 1; i++)
+	{
+  
+		for (j = 0; j < count - i - 1; j++)
+		{
+			if (name[j] > name[j + 1])
+			{
+				pop(&name[j], &name[j + 1]);
+			}
 
+		}
+	}
 }
