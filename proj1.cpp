22e5769f627783bbc1dabed82c226d29c7324b34
khobs99 <khobday99@gmail.com>
2020-01-25 16:52:32 -0500

    Update proj1.cpp

--- proj1.cpp
+++ proj1.cpp
@@ -3,39 +3,46 @@
 #include<fstream>
 #include<string>
 
-
+const int CAPACITY=20;
 using namespace std;
-#include "Person.h";
+#include "Person.h"
 
 
 void pop(string *xp, string *yp);
-void sort(string name[20], int count);
+void sort(string name[CAPACITY], int count);
 
 int main()
 {
-	class person names[20];
-	ifstream infile;
-	ofstream outfile;
-	string filename;
-	string name[20];
+    class person names[CAPACITY];
+    ifstream infile;
+    ofstream outfile;
+    string filename;
+    string name[CAPACITY];
+    int n=0;
 
-	cin >> filename;
-	infile.open(filename + ".txt");
-	outfile.open("Person_New.txt");
+    cin >> filename;
+    infile.open(filename + ".txt");
+    outfile.open("Person_New.txt");
 
-	if (infile.fail())
-	{
-		cout << "The file requested did not open" << endl;
-		exit(1);
-	}
-	while (!infile.eof())
-	{
-		names[20].get(infile);
-		
-	}
-	sort(name, 20);
-
-	
+    if (infile.fail())
+    {
+        cout << "The file requested did not open" << endl;
+        exit(1);
+    }
+    while (!infile.eof())
+    {
+        names[n].get(infile);
+        n++;
+    }
+    sort(name, CAPACITY);
+    for(int i=0;i<CAPACITY;i++)
+    {
+        names[i].put(outfile);
+    }
+    cout<<"The file has been created"<<endl;
+    
+    infile.close();
+    
 
 
 
@@ -43,23 +50,23 @@ int main()
 }
 void pop(string *xp, string *yp)
 {
-	string temp = *xp;
-	*xp = *yp;
-	*yp = temp;
+    string temp = *xp;
+    *xp = *yp;
+    *yp = temp;
 }
-void sort(string name[20], int count)
+void sort(string name[CAPACITY], int count)
 {
-	int i, j;
-	for (i = 0; i < count - 1; i++)
-	{
+    int i, j;
+    for (i = 0; i < count - 1; i++)
+    {
   
-		for (j = 0; j < count - i - 1; j++)
-		{
-			if (name[j] > name[j + 1])
-			{
-				pop(&name[j], &name[j + 1]);
-			}
+        for (j = 0; j < count - i - 1; j++)
+        {
+            if (name[j] > name[j + 1])
+            {
+                pop(&name[j], &name[j + 1]);
+            }
 
-		}
-	}
+        }
+    }
 }
