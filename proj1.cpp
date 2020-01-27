0e8d8bb6d3a2c72450d58d447adb1bae0abd51dc
khobs99 <khobday99@gmail.com>
2020-01-26 20:03:00 -0500

    Update proj1.cpp

--- proj1.cpp
+++ proj1.cpp
@@ -2,71 +2,73 @@
 #include<cstdlib>
 #include<fstream>
 #include<string>
+#include<iomanip>
 
-const int CAPACITY=20;
+const int CAPACITY = 20;
 using namespace std;
 #include "Person.h"
 
 
-void pop(string *xp, string *yp);
-void sort(string name[CAPACITY], int count);
+//void pop(string *xp, string *yp);
+//void sort(person name[], int count);
 
 int main()
 {
-    class person names[CAPACITY];
-    ifstream infile;
-    ofstream outfile;
-    string filename;
-    string name[CAPACITY];
-    int n=0;
+	class person names[CAPACITY];
+	ifstream infile;
+	ofstream outfile;
+	string filename;
+	int n = 0;
 
-    cin >> filename;
-    infile.open(filename + ".txt");
-    outfile.open("Person_New.txt");
+	cout << "Enter the file name you wish to open" << endl;
+	cin >> filename;
+	infile.open(filename + ".txt");
+	outfile.open("Person_New.txt");
 
-    if (infile.fail())
-    {
-        cout << "The file requested did not open" << endl;
-        exit(1);
-    }
-    while (!infile.eof())
-    {
-        names[n].get(infile);
-        n++;
-    }
-    sort(name, CAPACITY);
-    for(int i=0;i<CAPACITY;i++)
-    {
-        names[i].put(outfile);
-    }
-    cout<<"The file has been created"<<endl;
-    
-    infile.close();
-    
+	if (infile.fail())
+	{
+		cout << "The file requested did not open" << endl;
+		exit(1);
+	}
+	while (!infile.eof())
+	{
+		names[n].get(infile);
+		n++;
+	}
+	//sort(names, CAPACITY);
+	outfile << right << setw(20) << "first name" << setw(20) << "last name" << setw(20) << "age" << endl;
+	for (int i = 0; i < CAPACITY; i++)
+	{
+		names[i].put(outfile);
+	}
+	cout << "The file has been created" << endl;
+
+	infile.close();
+
 
 
 
 
 }
-void pop(string *xp, string *yp)
+/*void pop(string *xp, string *yp)
 {
-    string temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	string temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
-void sort(string name[CAPACITY], int count)
+void sort(person name[], int count)
 {
-    int i, j;
-    for (i = 0; i < count - 1; i++)
-    {
-  
-        for (j = 0; j < count - i - 1; j++)
-        {
-            if (name[j] > name[j + 1])
-            {
-                pop(&name[j], &name[j + 1]);
-            }
+	int i, j;
+	for (i = 0; i < count - 1; i++)
+	{
 
-        }
-    }
-}
+		for (j = 0; j < count - i - 1; j++)
+		{
+			if (name[j] > name[j + 1])
+			{
+				pop(&name[j], &name[j + 1]);
+			}
+
+		}
+	}
+}  */
