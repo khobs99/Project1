67a5b79d00b30072a400ece48b3dd22bc6180376
khobs99 <khobday99@gmail.com>
2020-01-27 21:08:07 -0500

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,3 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+#include<fstream>
+#include<string>
+#include<iomanip>
+
+const int CAPACITY=20;
+using namespace std;
+
+#include "Person.h"
+
 //Names constructor
 //Postcondition both first name and last name initialized to zero
 
@@ -10,15 +21,16 @@ person::person()
 
 }
 
-bool person::get(ifstream& in)
+bool person::get(istream& in)
 {
     in >> first_name >> last_name >> age;
-    return(in.good());
+	return(in.good());
 
 }
-void person::put(ofstream &out)
+void person::put(ostream &out)
 {
     out << right << first_name << setw(30) << last_name << setw(30) << age <<endl;
+	
 }
 bool person::operator<(person c1)
 {
